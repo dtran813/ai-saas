0dbce268d18fe46aae88fcd02dfe9bafbe0de783
dtran <dtran13899@gmail.com>
2023-07-28 18:19:39 -0600

    Create Dashboard's UI

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { TOOLS } from "@/utils/constant";
+
 const DashboardPage = () => {
+  const router = useRouter();
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
+      <div className="text-wrap-balance mb-8 space-y-4 text-center">
+        <h2 className="text-2xl font-bold md:text-4xl">
+          Empower Your Imagination with AI!
+        </h2>
+        <p className="text-sm font-light text-muted-foreground md:text-lg">
+          Unlock your creative potential and transforming imagination into
+          reality with the power AI
+        </p>
+      </div>
+      <div className="space-y-4 px-4 md:px-20 lg:px-32">
+        {TOOLS.map((tool) => (
+          <Card
+            key={tool.href}
+            onClick={() => router.push(tool.href)}
+            className="flex cursor-pointer items-center justify-between border-black/5 p-4 transition hover:shadow-md"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("w-fit rounded-md p-2", tool.bgColor)}>
+                <tool.icon className={cn("h-8 w-8", tool.color)} />
+              </div>
+              <p className="font-semibold">{tool.label}</p>
+            </div>
+            <ArrowRight className="h-5 w-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -52,3 +52,41 @@ export const ROUTES = [
     color: "",
   },
 ];
+
+export const TOOLS = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-600",
+    bgColor: "bg-pink-600/10",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-600",
+    bgColor: "bg-orange-600/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+  },
+];
